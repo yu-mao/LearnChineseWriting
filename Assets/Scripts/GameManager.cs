9b0539b615056ef27ef9e9a370876dd2de7de347
Jiayao Yu <yjybxx2@yahoo.com>
2024-12-10 23:17:46 +0100

    Got index finger position data when writing.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,15 +5,42 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private OVRHand _hand;
+    [SerializeField] private OVRSkeleton _skeleton;
+    [SerializeField] private GameObject _writingVisualFeedback;
     [SerializeField] private TextMeshProUGUI _debugText;
+
+    private bool _isWriting = false;
 
     public void IndexFingerStart()
     {
-        _debugText.text = "INDEX FINGER START";
+        _debugText.text = "INDEX FINGER WRITING";
+        _isWriting = true;
+        _writingVisualFeedback.SetActive(true);
     }
     
     public void IndexFingerEnd()
     {
-        _debugText.text = "INDEX FINGER END";
+        _debugText.text = "...";
+        _isWriting = false;
+        _writingVisualFeedback.SetActive(false);
+    }
+
+    private void Start()
+    {
+        _writingVisualFeedback.SetActive(false);
+    }
+    
+    private void Update()
+    {
+        if (_isWriting)
+        {
+            if (_hand.IsTracked && _skeleton.IsInitialized)
+            {
+                var indexFingerTip = _skeleton.Bones[(int)OVRSkeleton.BoneId.Hand_IndexTip];
+                Vector3 indexFingerTipPos = indexFingerTip.Transform.position;
+                _writingVisualFeedback.transform.position = indexFingerTipPos;
+            }
+        }
     }
 }
