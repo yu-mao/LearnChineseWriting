2a849bd9d87c8b2fb84d61cd8012fdc337eea0fc
Jiayao Yu <yjybxx2@yahoo.com>
2024-12-11 22:16:23 +0100

    Added line renderer to visualize writing feedback.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,28 +8,24 @@ public class GameManager : MonoBehaviour
     [SerializeField] private OVRHand _hand;
     [SerializeField] private OVRSkeleton _skeleton;
     [SerializeField] private GameObject _tempWritingVisualFeedback;
-    [SerializeField] private GameObject _tempIndexFingerTrackingFeedback;
-    [SerializeField] private int _writingWindowWidth = 4;
+    [SerializeField] private LineRenderer _lineRenderer;
     [SerializeField] private TextMeshProUGUI _debugText;
 
     private bool _isWriting = false;
-    private List<Vector3> _writingInCurrentWindow = new List<Vector3>();
-    private List<Vector3> _writingSampledPoints = new List<Vector3>();
     private List<Vector3> _writingVisualFeedback = new List<Vector3>();
 
     public void IndexFingerStart()
     {
-        // _debugText.text = "INDEX FINGER WRITING";
         _isWriting = true;
         _tempWritingVisualFeedback.SetActive(true);
     }
     
     public void IndexFingerEnd()
     {
-        // _debugText.text = "...";
         _isWriting = false;
         _tempWritingVisualFeedback.SetActive(false);
         _writingVisualFeedback.Clear();
+        _lineRenderer.positionCount = 0;
     }
 
     private void Start()
@@ -44,38 +40,13 @@ public class GameManager : MonoBehaviour
             if (_hand.IsTracked && _skeleton.IsInitialized)
             {
                 var indexFingerTipPos = GetIndexFingerTipPosition();
-                AverageWritingPoints(indexFingerTipPos);
-                
-                // temporary visual feedback for debug
-                _tempIndexFingerTrackingFeedback.transform.position = indexFingerTipPos;
+                _writingVisualFeedback.Add(indexFingerTipPos);
+                _lineRenderer.positionCount = _writingVisualFeedback.Count;
+                _lineRenderer.SetPositions(_writingVisualFeedback.ToArray());
             }
         }
     }
-
-    private void AverageWritingPoints(Vector3 indexFingerTipPos)
-    {
-        if (_writingInCurrentWindow.Count <= _writingWindowWidth)
-        {
-            _writingInCurrentWindow.Add(indexFingerTipPos);
-        }
-        else
-        {
-            Vector3 sum = Vector3.zero;
-            foreach (var writingPoint in _writingInCurrentWindow)
-            {
-                sum += writingPoint;
-            }
-            var averageWritingPoint = sum / _writingInCurrentWindow.Count;
-            
-            //TODO: delete debug
-            _tempWritingVisualFeedback.transform.position = averageWritingPoint;
-            _debugText.text = "Got an average point!";
-            
-            _writingSampledPoints.Add(averageWritingPoint);
-            _writingInCurrentWindow.Clear();
-        }
-    }
-
+    
     private Vector3 GetIndexFingerTipPosition()
     {
         // TODO: improve finger tip position tracking accuracy
