03b58239e31eebf923e135632f91dd35c7a04fdc
Jiayao Yu <yjybxx2@yahoo.com>
2024-12-12 01:17:26 +0100

    Added redo writing and finish writing hand pose recognitions.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,11 +8,12 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private OVRHand _hand;
     [SerializeField] private OVRSkeleton _skeleton;
+    [SerializeField] private List<LineRenderer> _writingVisualFeedbackGroup;
     [SerializeField] private int _segmentsPerCurve = 2; // interpolation factor, the bigger, the smoother & the slower
     [SerializeField] private TextMeshProUGUI _debugText;
 
     private bool _isWriting = false;
-    private LineRenderer _lineRenderer;
+    private int _countWritingVisualFeedbackDisplayed = 0;
     private List<Vector3> _writingPointsLogged = new List<Vector3>();
     private List<Vector3> _writingVisualFeedback = new List<Vector3>();
 
@@ -24,21 +25,34 @@ public class GameManager : MonoBehaviour
     
     public void IndexFingerEnd()
     {
+        if (_countWritingVisualFeedbackDisplayed >= _writingVisualFeedbackGroup.Count)
+        {
+            _countWritingVisualFeedbackDisplayed = 0;
+        }
+        else
+        {
+            _countWritingVisualFeedbackDisplayed += 1;
+        }
         _isWriting = false;
-        _lineRenderer.positionCount = 0;
-        _writingPointsLogged.Clear();
-        _writingVisualFeedback.Clear();
-        _debugText.text = "do a thumb-up to finish writing";
+        _debugText.text = "";
     }
 
     public void FinishWriting()
     {
+        ClearWriting();
         _debugText.text = "finish writing";
+    }
+
+
+    public void RedoWriting()
+    {
+        ClearWriting();
+        _debugText.text = "redo writing";
     }
 
     private void Start()
     {
-        _lineRenderer = GetComponent<LineRenderer>();
+        ClearWriting();
     }
 
     private void Update()
@@ -49,11 +63,21 @@ public class GameManager : MonoBehaviour
             {
                 var indexFingerTipPos = GetIndexFingerTipPosition();
                 _writingPointsLogged.Add(indexFingerTipPos);
-                if(_writingPointsLogged.Count >= 2)
-                    DrawSmoothLine(_writingPointsLogged.ToArray());
+                DrawSmoothLine(_writingPointsLogged.ToArray());
             }
         }
     }
+    
+    private void ClearWriting()
+    {
+        _writingPointsLogged.Clear();
+        foreach (var lineRenderer in _writingVisualFeedbackGroup)
+        {
+            lineRenderer.positionCount = 0;
+        }
+        _writingVisualFeedback.Clear();
+    }
+
     
     private Vector3 GetIndexFingerTipPosition()
     {
@@ -65,6 +89,8 @@ public class GameManager : MonoBehaviour
 
     private void DrawSmoothLine(Vector3[] points)
     {
+        if (_writingPointsLogged.Count < 2) return;
+        
         _writingVisualFeedback.Clear();
         
         for (int i = 0; i < points.Length - 1; i++)
@@ -87,14 +113,15 @@ public class GameManager : MonoBehaviour
         _writingVisualFeedback.Add(points[points.Length - 1]);
 
         // Apply the smoothed points to the LineRenderer
-        _lineRenderer.positionCount = _writingVisualFeedback.Count;
-        _lineRenderer.SetPositions(_writingVisualFeedback.ToArray());
+        _writingVisualFeedbackGroup[_countWritingVisualFeedbackDisplayed].positionCount = _writingVisualFeedback.Count;
+        _writingVisualFeedbackGroup[_countWritingVisualFeedbackDisplayed].SetPositions(_writingVisualFeedback.ToArray());
     }
-
-    //Returns a position between 4 Vector3 with Catmull-Rom spline algorithm
-    //http://www.iquilezles.org/www/articles/minispline/minispline.htm
+    
     private Vector3 CatmullRom(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
     {
+        //Returns a position between 4 Vector3 with Catmull-Rom spline algorithm
+        //http://www.iquilezles.org/www/articles/minispline/minispline.htm
+        
         //The coefficients of the cubic polynomial (except the 0.5f * which I added later for performance)
         Vector3 a = 2f * p1;
         Vector3 b = p2 - p0;
